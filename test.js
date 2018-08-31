34f034d8fb75bbadf0fcf5e9462f07379657f718
osdal <osdal67@gmail.com>
2018-08-31 11:54:32 +0300

    first

---
+++ test.js
@@ -0,0 +1,13 @@
+﻿function sayName (name) {
+	let message = 'My name is' + name
+	return message
+}
+
+let  assert = require('chai').assert
+
+describe('sayName', function () {
+
+	it("Получаем фразу с новым именем", function () {
+		assert.typeOf(sayName('ivan', 'string'))
+	})
+});
